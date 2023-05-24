6dc321e23574c267fed0f738abe65d3376857a13
Craig P Steffen <craigsteffen@gmail.com>
2023-05-24 07:41:32 -0500

    Set project title
    Setting the project title to something NCSA specific.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NCSA System Documentation'
+copyright = '2023, University of Illinois'
+author = 'Trustees of the University of Illinois'
 
 release = '0.1'
 version = '0.1.0'
